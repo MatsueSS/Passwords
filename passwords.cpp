db8033b4cbdb33c0b0c28544dc6c470aae3e2298
MatsueSS <147305790+MatsueSS@users.noreply.github.com>
2024-09-21 12:02:15 +0300

    Add files via upload

---
+++ passwords.cpp
@@ -0,0 +1,134 @@
+#include "passwords.h"
+
+#include <iostream>
+#include <cstdlib>
+#include <fstream>
+#include <algorithm>
+
+ErrorValues::ErrorValues(string msg) noexcept : msg(msg) {}
+ErrorValues::ErrorValues(const ErrorValues& obj) noexcept : msg(obj.msg) {}
+ErrorValues::~ErrorValues() {}
+const char* ErrorValues::what() const noexcept { return msg.c_str(); }
+
+Encrypt::Encrypt(string msg) : msg(msg) { _encryption(); }
+void Encrypt::_encryption(){
+    for(int i = 0; i < msg.size(); i++){
+        msg[i] += fault_num;
+        if(msg[i] > 126) msg[i] = 32 + (msg[i] - 127);
+    }
+    flag_crypt = 1;
+}
+Encrypt::Encrypt(const Encrypt& obj) : msg(obj.msg), flag_crypt(obj.flag_crypt) {}
+void Encrypt::_decryption(){
+    for(int i = 0; i < msg.size(); i++){
+        msg[i] -= fault_num;
+        if(msg[i] < 32) msg[i] = 127 - (32 - msg[i]);
+    }
+    flag_crypt = 0;
+}
+void Encrypt::getMsg(string& msg) { _decryption(); msg = this->msg; _encryption(); }
+void Encrypt::setMsg(string newMsg) { msg = newMsg; _encryption(); }
+string Encrypt::showMsg() const { return msg; }
+
+Password::Password(int min_l, int max_l, string chars) : min_l(min_l), max_l(max_l), chars(chars) { srand(time(nullptr)); }
+void Password::generatePassword(){
+    string psw = "";
+    int length = rand()%(max_l-min_l)+min_l;
+    for(int i = 0; i < length; i++){
+        psw += chars[rand()%chars.size()];
+    }
+    password.setMsg(psw);
+}
+Password::Password(Encrypt msg) : password(msg) {}
+string Password::printPassword() { string psw; password.getMsg(psw); return psw; }
+Encrypt Password::showPassword() const { return password; }
+/*virtual*/void Password::read(string fileName){
+    std::ifstream file(fileName);
+    if(file.is_open()){
+        string psw = "";
+        while(!file.eof()){
+            string line;
+            std::getline(file, line);
+            for(int i = 0, flag = 0; i < line.size(); i++){
+                if(flag == 1) psw += line[i];
+                if(line[i] == ' ') flag = 1;
+                if(flag == 0) name += line[i];
+            }
+        }
+        password.setMsg(psw);
+    }
+    else throw ErrorValues("File cannot be opened");
+    file.close();
+}
+/*virtual*/void Password::write(string fileName){
+    std::ofstream file(fileName);
+    if(file.is_open()){
+        file << name << " " << password.showMsg() << "\n";
+    }
+    else throw ErrorValues("File cannot be opened");
+    file.close();
+}
+
+LibPasswords::LibPasswords() {}
+void LibPasswords::addPassword(string name, Password psw){
+    libPsw.insert(libPsw.end(), {name, psw});
+}
+void LibPasswords::removePassword(string name){
+    libPsw.erase(libPsw.find(name));
+}
+/*virtual*/void LibPasswords::read(string fileName){
+    std::ifstream file(fileName);
+    if(file.is_open()){
+        while(!file.eof()){
+            string line, name = "", psw = "";
+            std::getline(file, line);
+            for(int i = 0, flag = 0; i < line.size(); i++){
+                if(flag == 1) psw += line[i];
+                if(line[i] == ' ') flag = 1;
+                if(flag == 0) name += line[i];
+            }
+            addPassword(name, Password(Encrypt(psw)));
+        }
+    }
+    else throw ErrorValues("File cannot be opened");
+    file.close();
+}
+/*virtual*/void LibPasswords::write(string fileName){
+    std::ofstream file(fileName);
+    if(file.is_open()){
+        std::map<string, Password>::iterator it = libPsw.begin();
+        while(it != libPsw.end()){
+            file << (*it).first << " " << (*it).second.showPassword().showMsg() << "\n";
+            it++;
+        }
+    }
+    else throw ErrorValues("File cannot be opened");
+    file.close();
+}
+string LibPasswords::operator[](int size){
+    if(size >= libPsw.size()) throw ErrorValues("Size must be less than size map");
+    std::map<string, Password>::iterator it = libPsw.begin();
+    for(int i = 0; i < libPsw.size() && i != size; i++) it++;
+    return (*it).first;
+}
+
+void swap(std::map<string, Password>::iterator a, std::map<string, Password>::iterator b){
+    std::map<string, Password>::iterator temp = a;
+    a = b;
+    b = temp;
+}
+
+void QuickSort(LibPasswords& lib, int low, int high, bool(*func)(string, string)){
+    if(low > high) return;
+    string p = lib[(low+high)/2];
+    int i = low, j = high;
+    while(i <= j){
+        while(func(p, lib[i])) i++;
+        while(func(lib[j], p)) j--;
+        if(i <= j){
+            swap(lib.libPsw.find(lib[i++]), lib.libPsw.find(lib[j--]));
+        }
+    }
+    QuickSort(lib, low, j, func);
+    QuickSort(lib, i, high, func);
+}
---
+++ passwords.h
@@ -0,0 +1,84 @@
+#ifndef _PASSWORDS_H_
+#define _PASSWORDS_H_
+
+#include <string>
+#include <map>
+
+using std::string;
+
+//not recommended edit this value
+enum { fault_num = 5 };
+
+class ErrorValues : public std::exception {
+protected:
+    string msg;
+
+public:
+    ErrorValues(string msg) noexcept;
+    ErrorValues(const ErrorValues& obj) noexcept;
+
+    virtual ~ErrorValues();
+    const char* what() const noexcept override;
+};
+
+class File {
+public:
+    virtual void read(string fileName) = 0;
+    virtual void write(string fileName) = 0;
+};
+
+class Encrypt {
+    string msg; // message for encrypt
+    bool flag_crypt = 0;
+
+    void _decryption(); // make decryption
+    void _encryption(); // make encryption
+
+public:
+    Encrypt(string msg = "");
+    Encrypt(const Encrypt& obj);
+
+    void getMsg(string& msg);
+    void setMsg(string newMsg);
+    string showMsg() const;
+};
+
+class Password : public File {
+    string name; // name of password
+    Encrypt password; // password
+    string chars; // chars for generate password  
+    int min_l, max_l; // length password
+
+public:
+    Password(int min_l = 20, int max_l = 21, string chars = "qwertuiopasdfghjklxcvbnm0123456789@$%&*!ABCDEFGHIJKLMNOPQRSTUVMXYZ");
+    Password(Encrypt msg);
+
+    void generatePassword();
+    Encrypt showPassword() const;
+    string printPassword();
+
+    virtual void read(string fileName) override;
+    virtual void write(string fileName) override;
+};
+
+class LibPasswords : public File {
+    std::map<string, Password> libPsw;
+
+    enum sort { _quick_sort = 0, _choice_sort = 1, _bubble_sort = 2, _pyramid_sort = 3, _insert_sort = 4 };
+
+public:
+    LibPasswords();
+
+    void removePassword(string name);
+    void addPassword(string name, Password psw);
+    void sortPasswords(sort type);
+    
+    string operator[](int size);
+
+    virtual void read(string fileName) override;
+    virtual void write(string fileName) override;
+
+    friend void QuickSort(LibPasswords& lib, int low, int high, bool(*func)(string, string));
+};
+
+#endif //_PASSWORDS_H_
