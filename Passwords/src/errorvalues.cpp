892d91345d5fe572f1f20ee9eb1975f729356d37
MatsueSS <147305790+MatsueSS@users.noreply.github.com>
2024-11-02 12:43:55 +0300

    Delete Passwords directory

--- Passwords/src/errorvalues.cpp
+++
@@ -1,9 +0,0 @@
-#include "errorvalues.h"
-
-ErrorValues::ErrorValues(string msg) noexcept : msg(msg) {}
-
-ErrorValues::ErrorValues(const ErrorValues& obj) noexcept : msg(obj.msg) {}
-
-ErrorValues::~ErrorValues() {}
-
-const char* ErrorValues::what() const noexcept { return msg.c_str(); }
